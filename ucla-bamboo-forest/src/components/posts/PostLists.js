774c25660d60436355e704f79dcde93e329e6392
Joonwon (Eric) Lee <joonwontech@gmail.com>
2023-02-28 13:47:18 -0800

    Favicon and filter button added
    *Filter button* needs to be implemented

--- ucla-bamboo-forest/src/components/posts/PostLists.js
+++ ucla-bamboo-forest/src/components/posts/PostLists.js
@@ -1,9 +1,25 @@
-import { Box, Text } from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import {
+    Box, Button, Menu, MenuButton, MenuList, MenuItem, Text
+} from "@chakra-ui/react";
 import Post from "./index";
 
 export default function PostLists({ posts }) {
     return (
         <Box paddingX={"200px"} align={"center"}>
+            <Box paddingX={"230px"} align={"left"}>
+                <Menu>
+                    <MenuButton as={Button} rightIcon={<HamburgerIcon />}>
+                        Sort by
+                    </MenuButton>
+                    <MenuList>
+                        <MenuItem>Sort by date</MenuItem>
+                        <MenuItem>Sort by most likes</MenuItem>
+                    </MenuList>
+                </Menu>
+            </Box>
+
+
             {posts?.length === 0 ? (
                 <Text textAlign={"center"} fontSize={"xl"} fontWeight={"bold"}>
                     No Posts Yet
