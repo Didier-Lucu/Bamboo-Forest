94b8bfed7edd060c8ddde3fa2efe3b7a70b126ac
Braeden Bethencourt <bbethencourt@ucla.edu>
2023-03-03 10:15:05 -0800

    Add ability to edit profile picture
      Add Change Profile Picture button to profile
      Add popup to choose file for profile image
      Add selected image preview
      Add function to update profile picture on backend

--- ucla-bamboo-forest/src/components/profile/Avatar.js
+++ ucla-bamboo-forest/src/components/profile/Avatar.js
@@ -1,14 +1,14 @@
 import { Avatar as ChakraAvatar } from "@chakra-ui/react"
 import { PROTECTED } from "lib/router"
 import { Link } from "react-router-dom"
-export default function Avatar({ user, size = "xl" }) {
+export default function Avatar({ user, size = "xl", overrideAvatar=null }) {
     if (!user) return "Loading ...";
     return (
         <ChakraAvatar
             as={Link}
             to={`${PROTECTED}/profile/${user.id}`}
             size={size}
-            src={user.profilePhoto}
+            src={overrideAvatar || user.avatar}
             _hover={{ cursor: "pointer", opacity: "0.8" }}
         />
 
--- ucla-bamboo-forest/src/components/profile/index.js
+++ ucla-bamboo-forest/src/components/profile/index.js
@@ -2,15 +2,19 @@ import PostsLists from "components/posts/PostLists";
 import { usePosts } from "hooks/Posts"
 import { useParams } from "react-router-dom";
 import { useUser } from "hooks/users";
-import { Divider, Flex, HStack, Stack, Text } from "@chakra-ui/react";
+import { useAuth } from "hooks/Auth";
+import { Divider, Flex, HStack, Stack, Text, Button, useDisclosure } from "@chakra-ui/react";
 import Avatar from "./Avatar";
 import { format } from "date-fns";
+import EditProfile from "./EditProfile"
 
 
 export default function Profile() {
     const { id } = useParams();
     const { posts, isLoading: postsLoading } = usePosts(id);
     const { user, isLoading: userLoading } = useUser(id);
+    const { user: authUser, isLoading: authLoading } = useAuth();
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
     if (userLoading) return "Loading...";
     
@@ -18,6 +22,13 @@ export default function Profile() {
         <Stack spacing="5">
             <Flex p={["4", "6"]} pos="relative" align="center">
                 <Avatar size="xl" user={user} />
+
+                {!authLoading && (authUser.id === user.id) &&
+                    <Button pos="absolute" mb="2" top="6" right="6" colorScheme="blue" onClick={onOpen}>
+                        Change Profile Picture
+                    </Button>}
+
+
                 <Stack ml="10">
                     <Text fontSize="xl">{user.username}</Text>
                     <HStack spacing="10">
@@ -32,6 +43,8 @@ export default function Profile() {
                         </Text>
                     </HStack>
                 </Stack>
+
+                <EditProfile isOpen={isOpen} onClose={onClose} />
             </Flex>
             <Divider />
             {postsLoading ? 
