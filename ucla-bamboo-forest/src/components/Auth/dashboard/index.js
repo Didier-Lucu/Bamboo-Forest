67cc95cb588204d14ec563d92b0de1baeea9826f
EricSeoungminKim <seoungminkim03@g.ucla.edu>
2023-03-08 01:52:12 -0800

    add posting auto-size and yarn

--- ucla-bamboo-forest/src/components/Auth/dashboard/index.js
+++ ucla-bamboo-forest/src/components/Auth/dashboard/index.js
@@ -2,55 +2,59 @@ import { Box, Button, Heading, HStack, Textarea } from "@chakra-ui/react";
 import PostLists from "components/posts/PostLists";
 import { useAuth } from "hooks/Auth";
 import { useAddPost, usePosts } from "hooks/Posts";
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
+import TextareaAutosize from "react-textarea-autosize";
 
 function NewPost() {
-    const { register, handleSubmit, reset } = useForm();
-    const { addPost, isLoading: addingPost } = useAddPost();
-    const { user, isLoading: authLoading } = useAuth();
+  const { register, handleSubmit, reset } = useForm();
+  const { addPost, isLoading: addingPost } = useAddPost();
+  const { user, isLoading: authLoading } = useAuth();
 
+  function handleAddPost(data) {
+    addPost({
+      uid: user.id,
+      text: data.text,
+    });
+    reset();
+  }
 
-    function handleAddPost(data) {
-        addPost({
-            uid: user.id,
-            text: data.text,
-        })
-        reset()
-    }
-
-    return (
-        <Box maxWidth={"600px"} margin={"auto"} paddingY={"20"}>
-            <form onSubmit={handleSubmit(handleAddPost)}>
-                <HStack justify={"space-between"}>
-                    <Heading size={"lg"}>What's on your mind?</Heading>
-                    <Button
-                        colorScheme={"red"}
-                        type={"submit"}
-                        isLoading={authLoading || addingPost} loadingText={"Loading"}>
-                        Post
-                    </Button>
-                </HStack>
-                <Textarea
-                    colorScheme={"red"}
-                    resize={"none"}
-                    marginTop={"5"}
-                    placeholder={"Create a new Post"}
-                    {...register("text", { required: true })}
-                />
-            </form>
-        </Box>
-    );
+  return (
+    <Box maxWidth={"600px"} margin={"auto"} paddingY={"20"}>
+      <form onSubmit={handleSubmit(handleAddPost)}>
+        <HStack justify={"space-between"}>
+          <Heading size={"lg"}>What's on your mind?</Heading>
+          <Button
+            colorScheme={"red"}
+            type={"submit"}
+            isLoading={authLoading || addingPost}
+            loadingText={"Loading"}
+          >
+            Post
+          </Button>
+        </HStack>
+        <Textarea
+          colorScheme={"red"}
+          as={TextareaAutosize}
+          resize={"none"}
+          marginTop={"5"}
+          placeholder={"Create a new Post"}
+          {...register("text", { required: true })}
+          minRows={4}
+        />
+      </form>
+    </Box>
+  );
 }
 
 export default function Dashboard() {
-    const { posts, isLoading } = usePosts();
+  const { posts, isLoading } = usePosts();
 
-    if (isLoading) return "Loading Posts"
+  if (isLoading) return "Loading Posts";
 
-    return (
-        <>
-            <NewPost />
-            <PostLists posts={posts} />
-        </>
-    );
+  return (
+    <>
+      <NewPost />
+      <PostLists posts={posts} />
+    </>
+  );
 }
