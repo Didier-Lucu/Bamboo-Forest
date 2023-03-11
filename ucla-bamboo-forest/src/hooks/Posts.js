7aac5bf7770f40b9f75fff39ba8e7ae0e1af26e0
EricSeoungminKim <seoungminkim03@g.ucla.edu>
2023-03-10 17:04:08 -0800

    added Category and Label
    If user doesn't select any of the categories, then they cannot post the postings.
    If user doesn't have any categories they want to select, they can make their own one.

--- ucla-bamboo-forest/src/hooks/Posts.js
+++ ucla-bamboo-forest/src/hooks/Posts.js
@@ -1,69 +1,87 @@
 import { useToast } from "@chakra-ui/react";
 import { uuidv4 } from "@firebase/util";
-import { arrayRemove, arrayUnion, collection, doc, orderBy, query, setDoc, updateDoc, where } from "firebase/firestore";
+import {
+  arrayRemove,
+  arrayUnion,
+  collection,
+  doc,
+  orderBy,
+  query,
+  setDoc,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import { db } from "lib/firebase";
 import { useState } from "react";
-import { useCollectionData, useDocumentData } from "react-firebase-hooks/firestore"
+import {
+  useCollectionData,
+  useDocumentData,
+} from "react-firebase-hooks/firestore";
 
 export function useAddPost() {
-    const [isLoading, setLoading] = useState(false);
-    const toast = useToast();
+  const [isLoading, setLoading] = useState(false);
+  const toast = useToast();
 
-    async function addPost(post) {
-        setLoading(true);
-        const id = uuidv4();
-        await setDoc(doc(db, "posts", id), {
-            ...post,
-            id,
-            date: Date.now(),
-            likes: [],
-        });
-        toast({
-            title: 'Post Added',
-            status: 'success',
-            duration: 2000,
-            position: "top",
-            isClosable: true,
-        })
-        setLoading(false)
-    }
-    return { addPost, isLoading };
+  async function addPost(post) {
+    setLoading(true);
+    const id = uuidv4();
+    await setDoc(doc(db, "posts", id), {
+      ...post,
+      id,
+      date: Date.now(),
+      likes: [],
+    });
+    toast({
+      title: "Post Added",
+      status: "success",
+      duration: 2000,
+      position: "top",
+      isClosable: true,
+    });
+    setLoading(false);
+  }
+  return { addPost, isLoading };
 }
 
-export function useToggleLike({id, isLiked, uid}) {
-    const [isLoading, setLoading] = useState(false);
+export function useToggleLike({ id, isLiked, uid }) {
+  const [isLoading, setLoading] = useState(false);
 
-    async function toggleLike() {
-        setLoading(true);
-        const docRef = doc(db, "posts", id)
-        await updateDoc(docRef, {
-            likes: isLiked ? arrayRemove(uid) : arrayUnion(uid),
-        });
-        setLoading(false);
-    }
+  async function toggleLike() {
+    setLoading(true);
+    const docRef = doc(db, "posts", id);
+    await updateDoc(docRef, {
+      likes: isLiked ? arrayRemove(uid) : arrayUnion(uid),
+    });
+    setLoading(false);
+  }
 
-    return { toggleLike, isLoading };
+  return { toggleLike, isLoading };
 }
 
-export function useDeletePost({id}) {
+export function useDeletePost({ id }) {
+  const [isLoading, setLoading] = useState(false);
 
-    const  [isLoading, setLoading] = useState(false);
+  async function deletePost() {}
 
-    async function deletePost() {}
-
-    return {deletePost, isLoading};
+  return { deletePost, isLoading };
 }
 
 export function usePost(id) {
-    const q = doc(db, "posts", id);
-    const [post, isLoading] = useDocumentData(q);
+  const q = doc(db, "posts", id);
+  const [post, isLoading] = useDocumentData(q);
 
-    return { post, isLoading };
+  return { post, isLoading };
 }
 
 export function usePosts(uid = null) {
-    const q = uid ? query(collection(db, "posts"), orderBy('date', 'desc'), where('uid', '==', uid)) : query(collection(db, "posts"), orderBy('date', 'desc'))
-    const [posts, isLoading, error] = useCollectionData(q);
-    if (error) throw error;
-    return { posts, isLoading };
+  const q = uid
+    ? query(
+        collection(db, "posts"),
+        orderBy("date", "desc"),
+        where("uid", "==", uid)
+      )
+    : query(collection(db, "posts"), orderBy("date", "desc"));
+  const [posts, isLoading, error] = useCollectionData(q);
+  if (error) throw error;
+  return { posts, isLoading };
 }
