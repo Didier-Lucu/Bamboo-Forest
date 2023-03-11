ff24fd228f8ee6b7ddffa8956f923e830d1b4d8d
Joonwon (Eric) Lee <joonwontech@gmail.com>
2023-03-10 16:51:15 -0800

    Google social login complete
    Currently google social login is complete
    
    Nickname is generated by unique-names-generator by andreasonny83

--- ucla-bamboo-forest/src/components/Auth/Login.js
+++ ucla-bamboo-forest/src/components/Auth/Login.js
@@ -1,18 +1,30 @@
 import { Box, Button, Heading, Center, Input, FormControl, FormLabel, FormErrorMessage, Text, Link } from '@chakra-ui/react';
 import { DASHBOARD, REGISTER } from 'lib/router';
 import { Link as RouterLink } from 'react-router-dom';
-import { useLogin } from 'hooks/Auth';
+import { useLogin, useSocialLogin } from 'hooks/Auth';
 import { useForm } from 'react-hook-form';
 import { emailValidate, passwordValidate } from 'utils/form-validate';
+import { FcGoogle } from 'react-icons/fc';
+
 
 export default function Login() {
     const { login, isLoading } = useLogin();
-    const { register, handleSubmit, reset, formState: {errors}} = useForm()
+    const { register, handleSubmit, reset, formState: { errors } } = useForm()
 
     console.log(errors)
 
     async function handleLogin(data) {
         const success = await login({ email: data.email, password: data.password, redirectTo: DASHBOARD });
+
+        if (success) {
+            reset();
+        }
+    }
+
+    const { socialLogin } = useSocialLogin(); // get socialLogin function from hook
+
+    async function handleSocialLogin(providerType) {
+        const success = await socialLogin({ redirectTo: DASHBOARD, providerType: providerType });
 
         if (success) {
             reset();
@@ -36,6 +48,19 @@ export default function Login() {
                 </FormControl>
                 <Button mt="4" type="submit" colorScheme="blue" size="md" width="full" isLoading={isLoading} loadingText="Welcome!" > Login </Button>
             </form>
+            <Button
+                w={'full'}
+                maxW={'md'}
+                mt={2}
+                variant={'outline'}
+                leftIcon={<FcGoogle />}
+                onClick={() => handleSocialLogin('google')}
+            >
+                <Center>
+                    <Text>Sign in with Google</Text>
+                </Center>
+            </Button>
+
             <Text align="center">
                 Don't have an account?{" "}
                 <Link as={RouterLink} to={REGISTER} color="blue.400" fontWeight="medium" textDecor="underline" _hover={{ background: "cyan.100" }} >Register</Link>
