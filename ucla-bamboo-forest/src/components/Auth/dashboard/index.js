2216065917efda78130828d81726a9b17342f7a5
DidierLucu <86886766+DidierLucu@users.noreply.github.com>
2023-03-11 20:36:10 -0800

    Add upload and remove upload button
    In previous commit merge the upload button was removed. Added it back
    and formated page. When you upload you are able to remove the upload and
    reupload somthing else if you want.

--- ucla-bamboo-forest/src/components/Auth/dashboard/index.js
+++ ucla-bamboo-forest/src/components/Auth/dashboard/index.js
@@ -99,8 +99,7 @@ function NewPost() {
           {...register("text", { required: true })}
           minRows={3}
         />
-   
-    
+
         <HStack marginTop={"5"}>
           <Heading size={"sm"} color={"gray"}>
             Choose Your Category
@@ -123,12 +122,45 @@ function NewPost() {
                 value={category}
               >
                 {category}
+
+                
               </Button>
             ))}
             <Button size="sm" onClick={handleAddCategory}>
               +
             </Button>
+
+            <div style={{ display: 'flex', marginLeft: 'auto'}}>
+              <Button size="sm" as="label" htmlFor="file-input"> 
+                Upload
+                <input
+                  id="file-input"
+                  type="file"
+                  accept="image/*"
+                  onChange={handleChange}
+                  hidden
+                />
+              </Button>
+            </div>
           </HStack>
+          {file && (
+        <HStack spacing={2} mt={2} justifyContent="space-between">
+          <Button
+            backgroundColor={theme.colors.red[400]}
+            ml={"430px"}
+            size="sm"
+            marginRight={2}
+            onClick={() => {
+              setFile(null);
+            }}
+          >
+            Delete
+          </Button>
+          <div style={{ marginLeft: 'auto', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', textAlign: 'right'}}>{file.name}</div>
+        </HStack>
+      )}
+    
+              
         </Box>
       </form>
     </Box>
