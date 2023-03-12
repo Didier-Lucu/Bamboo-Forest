bfdfaae341256941eb84cdcf02a80ed8c29a5e73
DidierLucu <86886766+DidierLucu@users.noreply.github.com>
2023-03-11 17:25:39 -0800

    Add ability to insert images on post
    Added button to insert an image, image is added to storeage in firebase
    and a file image. If an image exists for a post it is then displayed
    under the text.

--- ucla-bamboo-forest/src/components/Auth/dashboard/index.js
+++ ucla-bamboo-forest/src/components/Auth/dashboard/index.js
@@ -2,6 +2,7 @@ import { Box, Button, Heading, HStack, Textarea } from "@chakra-ui/react";
 import PostLists from "components/posts/PostLists";
 import { useAuth } from "hooks/Auth";
 import { useAddPost, usePosts } from "hooks/Posts";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import TextareaAutosize from "react-textarea-autosize";
 
@@ -9,14 +10,19 @@ function NewPost() {
   const { register, handleSubmit, reset } = useForm();
   const { addPost, isLoading: addingPost } = useAddPost();
   const { user, isLoading: authLoading } = useAuth();
-
+  const [ file, setFile ] = useState(null);
   function handleAddPost(data) {
     addPost({
       uid: user.id,
       text: data.text,
+      image: file,
     });
     reset();
   }
+  function handleChange(e) {
+    setFile(e.target.files[0]);
+  }
+
 
   return (
     <Box maxWidth={"600px"} margin={"auto"} paddingY={"20"}>
@@ -41,6 +47,9 @@ function NewPost() {
           {...register("text", { required: true })}
           minRows={4}
         />
+    
+        <input type="file" accept="image/*" onChange={handleChange} />
+          
       </form>
     </Box>
   );
--- ucla-bamboo-forest/src/hooks/Posts.js
+++ ucla-bamboo-forest/src/hooks/Posts.js
@@ -1,23 +1,47 @@
 import { useToast } from "@chakra-ui/react";
+import { getDownloadURL, ref, uploadBytes } from "@firebase/storage";
 import { uuidv4 } from "@firebase/util";
 import { arrayRemove, arrayUnion, collection, doc, orderBy, query, setDoc, updateDoc, where } from "firebase/firestore";
-import { db } from "lib/firebase";
+import { db, storage } from "lib/firebase";
 import { useState } from "react";
 import { useCollectionData, useDocumentData } from "react-firebase-hooks/firestore"
 
 export function useAddPost() {
     const [isLoading, setLoading] = useState(false);
+    //const [ file, setFile ] = useState(null);
     const toast = useToast();
-
     async function addPost(post) {
         setLoading(true);
         const id = uuidv4();
+        const file = await imageUpload(id)
+
+        async function imageUpload(id) {
+            if (!post.image) {
+
+                return;
+            }
+    
+            const fileRef = ref(storage, "postImages/" + id);
+            await uploadBytes(fileRef, post.image);
+            const file = await getDownloadURL(fileRef)
+    
+            toast({ title: "Image uploaded!", status: "success",
+                isClosable: true, position: "top", duration: 5000 });
+    
+                
+            return file
+        }
         await setDoc(doc(db, "posts", id), {
-            ...post,
+            uid: post.uid,
+            text: post.text,
             id,
             date: Date.now(),
             likes: [],
+            image: file,
         });
+       // const docRef = doc(db, "posts", id)
+      //  await updateDoc(docRef, { iceimage: file })
+        
         toast({
             title: 'Post Added',
             status: 'success',
@@ -25,10 +49,15 @@ export function useAddPost() {
             position: "top",
             isClosable: true,
         })
+        
         setLoading(false)
     }
     return { addPost, isLoading };
 }
+
+
+  
+
 
 export function useToggleLike({id, isLiked, uid}) {
     const [isLoading, setLoading] = useState(false);
