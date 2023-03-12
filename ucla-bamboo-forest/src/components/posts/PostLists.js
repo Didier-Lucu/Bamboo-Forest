f2bbe68692811acc462503889c99e95f2807d008
Hae Won Cho <>
2023-03-12 15:28:36 -0700

    Add sort by likes
    The posts under Home and Profile can be sorted by likes

--- ucla-bamboo-forest/src/components/posts/PostLists.js
+++ ucla-bamboo-forest/src/components/posts/PostLists.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import {
   Box,
@@ -11,28 +12,42 @@ import {
 import Post from "./index";
 
 export default function PostLists({ posts }) {
+  const [sortBy, setSortBy] = useState("");
+
+  function sortedPosts(posts) {
+    if (sortBy === "most-popular") {
+      return posts.sort((a, b) => b.likes.length - a.likes.length);
+    }
+    else if (sortBy === "least-popular") {
+      return posts.sort((a, b) => a.likes.length - b.likes.length);
+    }
+    else {
+      return posts;
+    }
+  }
+
   return (
     <Box paddingX={"200px"} align={"center"}>
       <Box paddingX={"60px"} align={"left"}>
         <Menu>
           <MenuButton as={Button} rightIcon={<HamburgerIcon />}>
-            Sort by
+            Sort by {sortBy === "" ? "(Default)" : ""}
           </MenuButton>
           <MenuList>
             <MenuItem>Sort by Date</MenuItem>
             <MenuItem>Sort by Category</MenuItem>
-            <MenuItem>Sort by Most Likes</MenuItem>
-            <MenuItem>Sort by Least Likes</MenuItem>
+            <MenuItem onClick={() => setSortBy("most-popular")}>Sort by Most Likes</MenuItem>
+            <MenuItem onClick={() => setSortBy("least-popular")}>Sort by Least Likes</MenuItem>
           </MenuList>
         </Menu>
       </Box>
 
-      {posts?.length === 0 ? (
+      {sortedPosts(posts)?.length === 0 ? (
         <Text textAlign={"center"} fontSize={"xl"} fontWeight={"bold"}>
           No Posts Yet
         </Text>
       ) : (
-        posts?.map((post) => <Post key={post.id} post={post} />)
+        sortedPosts(posts)?.map((post) => <Post key={post.id} post={post} />)
       )}
     </Box>
   );
