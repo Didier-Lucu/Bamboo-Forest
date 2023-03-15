98a332c746a532f9b705481ce6aa67d6713c4fce
DidierLucu <86886766+DidierLucu@users.noreply.github.com>
2023-03-15 15:39:12 -0700

    Add second firebase
    Needed a second firebase trial for testing.

--- ucla-bamboo-forest/src/lib/firebase.js
+++ ucla-bamboo-forest/src/lib/firebase.js
@@ -3,6 +3,8 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
+/*
+//Main one
 const firebaseConfig = {
     apiKey: "AIzaSyDkJoECKNeJfWoCh8MJ-L2Zs0-4MdoGl-g",
     authDomain: "ucla-bamboo-forest.firebaseapp.com",
@@ -12,7 +14,17 @@ const firebaseConfig = {
     appId: "1:920132615060:web:0815f6660ee537853ced81",
     measurementId: "G-5HBY8TTWZN"
 };
-
+*/
+//Second firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyAms3E9LVUHjRK6NPoD7b2fHJICVusnqMA",
+    authDomain: "ucla-bamboo-forest-v2.firebaseapp.com",
+    projectId: "ucla-bamboo-forest-v2",
+    storageBucket: "ucla-bamboo-forest-v2.appspot.com",
+    messagingSenderId: "381926331081",
+    appId: "1:381926331081:web:a2d5ae71766ffa0b203318",
+    measurementId: "G-2MQ1N14ETC"
+  };
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
